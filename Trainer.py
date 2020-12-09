015e64381b62ce6b5aabf28db1829961a1655d1d
haroldrubio <harrywill.rubio@gmail.com>
2020-12-08 21:37:06 -0500

    Move to set-prime paradigm

--- Trainer.py
+++ Trainer.py
@@ -25,7 +25,8 @@ class Trainer:
     
     Variables:
         BATCH_SIZE: Size of each batch
-        model: Inherits torch.nn.Module
+        model: An instance that inherits torch.nn.Module
+        model: A class that inherits torch.nn.Module
         device: Determines which device the model and loaded data will be on
         dtype: The preferred data type for the model and data
         paths['type']: Directories to be accessed/written to
@@ -39,12 +40,12 @@ class Trainer:
         device: The device that is being used
         activations: Dictionary containing layer activations (see single_pass)
     """
-    def __init__(self, model, data_path, device, batch_size=1):
+    def __init__(self, model_class, data_path, device, batch_size=1):
         """
         Create a Trainer object
 
         Args:
-            model(torch.nn.Module): A PyTorch model that inherits torch.nn.Module
+            model_class(torch.nn.Module): A PyTorch class that inherits torch.nn.Module
             data_path(str): Relative directory containing the data
                             Contains /train and /val directories
             device(torch.device): The device to use for the model and data
@@ -54,7 +55,9 @@ class Trainer:
         self.NUM_VAL = 5000
         self.NUM_TEST = 5000
         # Store the PyTorch Model
-        self.model = model
+        self.model_class = model_class
+        self.model_args, self.model_kwargs = None, None
+        self.model = None
         self.device = device
         self.dtype = torch.float32
         # Generate and store data paths
@@ -74,10 +77,15 @@ class Trainer:
         self.test_loader = DataLoader(self.test_set, batch_size=self.BATCH_SIZE, shuffle=True)
         # Initialize a Hyperparameter object
         self.hyp = None
+        self.arch_hyp = None
         # Define None objects for criterion, optimizer, and scheduler
+        self.optimizer_class = None
+        self.optimizer_args, self.optimizer_kwargs = None, None
         self.optimizer = None
-        self.criterion = None
+        self.scheduler_class = None
+        self.scheduler_args, self.scheduler_kwargs = None, None
         self.scheduler = None
+        self.criterion = None
         # Define dictionary of empty lists for histories
         self.history = {}
         self.init_history()
@@ -161,7 +169,9 @@ class Trainer:
         if os.path.isdir(self.paths['checkpoints']):
             shutil.rmtree(self.paths['checkpoints'])
         os.mkdir(self.paths['checkpoints'])
-        # For epochs
+
+        # Instantiate a model to train and move it to the device
+        self.create_model()
         for e in range(epochs):
         #   If first epoch, get initial evaluations
             if e == 0:
@@ -300,34 +310,181 @@ class Trainer:
         ret_dict['preds'] = preds
         ret_dict['loss'] = loss
         return ret_dict
+
+    def create_object(self, object_type=None):
+        """
+        Helper function to create objects within the trainer
+        """
+        # First parse the object type
+        obj_class, args, kwargs, hyp = None, None, None, None
+        if object_type == 'model':
+            obj_class, args, kwargs, hyp = self.model_class, self.model_args, self.model_kwargs, self.arch_hyp
+        elif object_type == 'optim':
+            obj_class, optim_args, kwargs, hyp = self.optimizer_class, self.optimizer_args, self.optimizer_kwargs, self.hyp
+        #   Prepend model parameters to arguments
+            optim_args = list(optim_args)
+            args = [self.model.parameters()]
+            args.extend(optim_args)
+            args = tuple(args)
+        # Transform empty arguments into none
+        if len(args) == 0:
+            args = None
+        if len(kwargs) == 0:
+            kwargs = None
+        
+        # Perform object creation
+        ret_obj = None
+        # Obtain a hyperparameter sample
+        param_sample = None
+        if hyp is not None:
+            param_sample = hyp.sample()
+        #   Check for kwargs - if not none, merge the hyperparameter sample
+            if kwargs is not None:
+                kwargs.update(param_sample)
+
+        # Choose which combination of parameters to instantiate with
+        if hyp is None:
+            if args is None and kwargs is not None:
+                ret_obj = obj_class(**kwargs)
+            elif args is not None and kwargs is None:
+                ret_obj = obj_class(*args)
+            elif args is None and kwargs is None:
+                ret_obj = obj_class()
+            else:
+                ret_obj = obj_class(*args, **kwargs)
+        else:
+            if args is None and kwargs is None:
+                ret_obj = obj_class(**param_sample)
+            elif args is None and kwargs is not None:
+                ret_obj = obj_class(**kwargs)
+            elif args is not None and kwargs is None:
+                ret_obj = obj_class(*args, **param_sample)
+            else:
+                ret_obj = obj_class(*args, **kwargs)
+        return ret_obj
+    def create_model(self):
+        """
+        Creates an instantiation of the model using the given hyperparameters and the primed
+        hyperparameters
+        """
+        # If the model has not been primed and no hyperparameter object has been provided, throw error
+        if self.model_args is None and self.model_kwargs is None and self.arch_hyp is None:
+            print('ERR: Model not primed')
+            sys.exit(1)
+        
+        # Create a model
+        self.model = self.create_object(object_type='model')
+        self.model.to(self.device)
+
+        # Then, instantiate the optimizer
+        self.create_optimizer()
+
+        # Optionally, if using a scheduler, instantiate a scheduler
+        self.create_scheduler()
+    def create_optimizer(self):
+        """
+        Creates an instantiation of an optimizer using the given hyperparameters and the primed
+        hyperparameters
+        """
+        # If the optimizer was not primed and no optimizer hyperparameters were provided, throw error
+        if self.optimizer_args is None and self.optimizer_kwargs is None and self.hyp is None:
+            print('ERR: Optimizer not primed')
+            sys.exit(1)
+        
+        self.optimizer = self.create_object(object_type='optim')
+    def create_scheduler(self):
+        """
+        Creates an instantiation of scheduler using the primed hyperparameters
+        """
+        # If the scheduler was not primed throw error
+        if self.scheduler_args is None and self.scheduler_kwargs is None:
+            print('ERR: Scheduler not primed')
+            sys.exit(1)
+        
+        # For empty argument, set to none
+        if len(self.scheduler_args) == 0:
+            self.scheduler_args = None
+        if len(self.scheduler_kwargs) == 0:
+            self.scheduler_kwargs = None
+
+        # Choose which combination of parameters to instantiate with
+        if self.scheduler_args is not None and self.scheduler_kwargs is None:
+            self.scheduler = self.scheduler_class(self.optimizer, *self.scheduler_args)
+        elif self.scheduler_args is None and self.scheduler_kwargs is not None:
+            self.scheduler = self.scheduler_class(self.optimizer, **self.scheduler_kwargs)
+        elif self.scheduler_args is None and self.scheduler_kwargs is None:
+            self.scheduler = self.scheduler_class(self.optimizer)
+        else:
+            self.scheduler = self.scheduler_class(self.optimizer, *self.scheduler_args, **self.scheduler_kwargs)
+    def prime_model(self, *args, **kwargs):
+        """
+        Stores the model parameters to be used upon each initialization
+
+        Args:
+            args, kwargs: Static hyperparameters that will be used each time a model
+                          is instantiated
+        """
+        self.model_args = args
+        self.model_kwargs = kwargs
+    def prime_optimizer(self, *args, **kwargs):
+        """
+        Stores the optimizer parameters to be used upon each initialization
+        
+        Args:
+            args, kwargs: Static hyperparameters that will be used each time an optimizer
+                          is instantiated
+        """
+        self.optimizer_args = args
+        self.optimizer_kwargs = kwargs
+    def prime_scheduler(self, *args, **kwargs):
+        """
+        Stores the scheduler parameters to be used upon each initialization
+        
+        Args:
+            args, kwargs: Static hyperparameters that will be used each time a scheduler
+                          is instantiated
+        """
+        self.scheduler_args = args
+        self.scheduler_kwargs = kwargs
     def set_hyperparameters(self, hyp):
         """
-        Set the local Hyperparameters object (see: Hyperparameters.py)
+        Set the local Hyperparameters object for the optimizer (see: Hyperparameters.py)
 
         Args:
             hyp(Hyperparameters): Redefine the local Hyperparameters object
         -Set the local Hyperparameters object
         """
         self.hyp = hyp
-    def set_optimizer(self, optimizer_class, params):
+    def set_arch_hyperparameters(self, hyp):
         """
-        Given the class of a PyTorch optimizer, and a dictionary of parameters, set the local optimizer
+        Set the local Hyperparameters object for the model (see: Hyperparameters.py)
+
+        Args:
+            hyp(Hyperparameters): Redefine the local Hyperparameters object
+        """
+        self.arch_hyp = hyp
+    def set_optimizer(self, optimizer_class):
+        """
+        Given the class of a PyTorch optimizer, set the local optimizer to create off of
         Args:
             optimizer_class(torch.optim): The class of a desired optimizer
-            params(dict): Dictionary of optimizer hyperparameters (hyp: value)
         """
-        self.model.to(device=self.device)
-        self.optimizer = optimizer_class(self.model.parameters(), **params)
-    def set_scheduler(self, scheduler_class, **kwargs):
+        self.optimizer_class = optimizer_class
+    def set_model(self, model_class):
         """
-        Sets a local scheduler - requires the optimizer to have been set
+        Given the class of a PyTorch model(from nn.Module), create models based off of this class
+        Args:
+            model_class(torch.nn.Module): The class of the model to train
+        """
+        self.model_class = model_class
+    def set_scheduler(self, scheduler_class):
+        """
+        Sets a local scheduler class
 
         Args:
-            scheduler_class(torch.optim.lr_scheduler): A scheduler class to be instantiated with the passed in args
-            params(dict): Dictionary of scheduler hyperparameters (hyp: value)
+            scheduler_class(torch.optim.lr_scheduler): A scheduler class to be instantiated later
         """
-        self.scheduler = scheduler_class(self.optimizer, **kwargs)
-
+        self.scheduler_class = scheduler_class
     def set_criterion(self, criterion_class):
         """
         Set the local loss function as the desired loss function
